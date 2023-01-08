c2c129a372c1229a91949c29f7bb76900dc85361
philipz <philipzheng@gmail.com>
2023-01-08 11:01:19 +0800

    Update LoginHero.jsx

--- src/components/LoginHero.jsx
+++ src/components/LoginHero.jsx
@@ -15,7 +15,7 @@ function Hero({handleLogIn}) {
             <br />
             <FlexCenter>
             <MarketingButton alt="pizza" primary onClick={handleLogIn}>
-              Login with GitHub 20230108
+              Login with GitHub
             </MarketingButton>
           </FlexCenter>
           </FlexColumnCenter>
